22eb9527b2f472ab7c848ba42ca99085f0a213bb
Chris Shao <bjchris32@gmail.com>
2024-07-30 17:15:39 -0500

    test: add unit test for model

--- server/test/models/habit.test.ts
+++ server/test/models/habit.test.ts
@@ -1,13 +1,55 @@
-import { add, multiply } from "../../src/models/habit";
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { Habit } from "../../src/models/habit";
 
-describe("Math functions", () => {
-  it("should multiply 5 by 3", () => {
-    const result = multiply(5, 3);
-    expect(result).toEqual(15);
+
+let mongoServer: MongoMemoryServer;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const mongoUri = await mongoServer.getUri();
+
+  await mongoose.connect(mongoUri);
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
+
+describe('Habit model test', () => {
+  it('should create & save habit successfully', async () => {
+    const habitData = { name: 'Habit 1' };
+    const habit = new Habit(habitData);
+    const savedHabit = await habit.save();
+
+    expect(savedHabit._id).toBeDefined();
+    expect(savedHabit.name).toBe(habitData.name);
   });
 
-  it("should add 5 by 3", () => {
-    const result = add(5, 3);
-    expect(result).toEqual(8);
-  });
+//   it('should not create user with invalid field', async () => {
+//     const userWithInvalidField = new User({ name: 'John Doe', email: 'john@example.com', nickname: 'Johnny' });
+//     let err;
+//     try {
+//       const savedUserWithInvalidField = await userWithInvalidField.save();
+//       err = savedUserWithInvalidField;
+//     } catch (error) {
+//       err = error;
+//     }
+//     expect(err).toBeInstanceOf(mongoose.Error.ValidationError);
+//     expect(err.errors.nickname).toBeUndefined();
+//   });
+
+//   it('should not create user without required field', async () => {
+//     const userWithoutRequiredField = new User({ name: 'John Doe' });
+//     let err;
+//     try {
+//       const savedUserWithoutRequiredField = await userWithoutRequiredField.save();
+//       err = savedUserWithoutRequiredField;
+//     } catch (error) {
+//       err = error;
+//     }
+//     expect(err).toBeInstanceOf(mongoose.Error.ValidationError);
+//     expect(err.errors.email).toBeDefined();
+//   });
 });
