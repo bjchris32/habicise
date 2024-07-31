05c5211d36c4138f09280d58b5fcc3b0db417698
Chris Shao <bjchris32@gmail.com>
2024-07-31 17:07:45 -0500

    feat(controller): add habits controller

---
+++ server/src/controllers/habits.ts
@@ -0,0 +1,30 @@
+import { Habit } from "../../src/models/habit";
+import { Request, Response } from 'express';
+import assert from 'assert';
+
+export const createHabit = async (req: Request, res: Response) => {
+  try {
+    const habit = new Habit(req.body);
+    const savedHabit = await habit.save();
+    res.status(201).json(savedHabit);
+  } catch (error) {
+    assert(error instanceof Error)
+
+    if (error.name === 'ValidationError') {
+      return res.status(400).json({ message: error.message });
+    }
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getHabit = async (req: Request, res: Response) => {
+  try {
+    const habit = await Habit.findById(req.params.id);
+    if (!habit) return res.status(404).json({ message: 'Habit not found' });
+    res.status(200).json(habit);
+  } catch (error) {
+    assert(error instanceof Error)
+
+    res.status(400).json({ message: error.message });
+  }
+};
