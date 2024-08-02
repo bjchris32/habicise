b6fdda05861dd1069939434d70eadc03af88b99c
Chris Shao <bjchris32@gmail.com>
2024-08-02 17:41:56 -0500

    test: use the routes and add test for update

--- server/test/controllers/habits.test.ts
+++ server/test/controllers/habits.test.ts
@@ -3,15 +3,14 @@
 import { createHabit, getHabit } from "../../src/controllers/habits";
 import { dbConnect, dbDisconnect } from "../utils/dbHandler"
 import express, { Express, Request, Response } from "express";
+import router from '../../src/config/routes';
 import request from 'supertest';
 import { Habit } from "../../src/models/habit";
 
+// TODO: put into test util to set up routes
 const app = express();
 app.use(express.json());
-app.post('/habits', createHabit);
-app.get('/habits/:id', getHabit);
-// app.put('/habits/:id', updateHabit);
-// app.delete('/habits/:id', deleteHabit);
+app.use('/api', router);
 
 beforeAll(async () => {
   await dbConnect();
@@ -24,7 +23,7 @@ afterAll(async () => {
 describe("Habits functions", () => {
   it("should create a habit document", async () => {
     const response = await request(app)
-      .post('/habits')
+      .post('/api/habits')
       .send({ name: 'Habit 2' });
     expect(response.status).toBe(201);
     expect(response.body.name).toBe('Habit 2');
@@ -33,12 +32,19 @@ describe("Habits functions", () => {
   it("should read a habit document", async () => {
     const habit = new Habit({ name: 'Habit 3' });
     const savedHabit = await habit.save();
-    const response = await request(app).get(`/habits/${savedHabit._id}`);
+    const response = await request(app).get(`/api/habits/${savedHabit._id}`);
     expect(response.status).toBe(200);
     expect(response.body.name).toBe('Habit 3');
   });
 
-  it("should update a habit document", () => {
+  it("should update a habit document", async () => {
+    const habit = new Habit({ name: 'Habit 3' });
+    const savedHabit = await habit.save();
+    const response = await request(app)
+      .put(`/api/habits/${savedHabit._id}`)
+      .send({ name: 'Habit not your business' });;
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe ('Habit not your business');
   });
 
   it("should delete a habit document", () => {
