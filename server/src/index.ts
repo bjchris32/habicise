0be2ad6c367a748e301d73b593fd2ab676fbdc11
Chris Shao <bjchris32@gmail.com>
2024-08-11 18:25:42 -0500

    feat(server): middleware integration

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import mongoose from 'mongoose';
 import router from './config/routes';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -14,7 +15,32 @@ mongoose.connect(mongoURI).then(() => {
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+const winston = require("winston");
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "logs/app.log" }),
+  ],
+});
+
+app.use((req, res, next) => {
+  // Log an info message for each incoming request
+  logger.info(`Received a ${req.method} request for ${req.url}`);
+  next();
+});
+
+app.use((err: any, req: any, res: any, next: any) => {
+  // Log the error message at the error level
+  logger.error(err.message);
+  res.status(500).send();
+});
+
 app.get("/", (req: Request, res: Response) => {
+  logger.log("info", "Request received: ", req);
   res.send("Express + TypeScript Server");
 });
 
