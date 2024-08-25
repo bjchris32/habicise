ad3861e4e53fe2886e705795c9228f3a9575d88e
Chris Shao <bjchris32@gmail.com>
2024-08-25 14:47:54 -0500

    feat(commit list): conditionaly rendering

--- client/src/components/CommitList.tsx
+++ client/src/components/CommitList.tsx
@@ -6,18 +6,27 @@ interface CommitListProps {
 }
 
 const CommitList: React.FC<CommitListProps> = ({ commits }) => {
-  return (
-    <div>
-      <h4>Commits</h4>
-      <ul>
-        {commits?.map((commit) => (
-          <li key={commit._id}>
-            {new Date(commit.createdAt as string).toString()}, {commit.description} with time: {commit.length}
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+  if (commits && commits.length > 0) {
+    return (
+      <div>
+        <h4>Commits</h4>
+        <ul>
+          {commits?.map((commit) => (
+            <li key={commit._id}>
+              {new Date(commit.createdAt as string).toString()}, {commit.description} with time: {commit.length}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  } else {
+    return (
+      <>
+        add your first commit!
+      </>
+    )
+  }
+
 }
 
 export default CommitList;
