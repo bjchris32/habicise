bc70d3de85e3be8b8e169309141a47b28ca14ae2
Chris Shao <bjchris32@gmail.com>
2024-08-29 12:17:36 -0500

    feat: error handling on the form

--- client/src/components/CommitForm.tsx
+++ client/src/components/CommitForm.tsx
@@ -12,14 +12,23 @@ interface CommitFormProps {
 
 const CommitForm: React.FC<CommitFormProps> = ({ onSave, habit }) => {
   const [commit, setCommit] = useState<ICommit>({ description: '', length: 0 });
+  const [error, setError] = useState(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setCommit((prevCommit) => ({ ...prevCommit, [name]: value }));
+    if (e.target.value.trim() !== '') {
+      setError(false);  // Reset error if input is not empty
+    }
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (commit.description.trim() === '') {
+      setError(true);  // Show error if input is empty
+      return
+    }
+
     if (!!habit._id) {
       await createCommit({...commit, habit: habit._id});
       setCommit({ description: '', length: 0 });
@@ -32,6 +41,7 @@ const CommitForm: React.FC<CommitFormProps> = ({ onSave, habit }) => {
       component="form"
       sx={{
         display: 'flex',      // Flexbox container
+        alignItems: 'center',
         gap: 1               // Space between elements
       }}
       onSubmit={handleSubmit}
@@ -40,10 +50,12 @@ const CommitForm: React.FC<CommitFormProps> = ({ onSave, habit }) => {
     >
       <TextField
         required
-        label="description"
+        label="Describe what you did today"
         name="description"
         value={commit.description}
         onChange={handleChange}
+        error={error}
+        helperText={error ? 'Commit description is required' : ''}  // Show error message
       />
       <TextField
         required
--- client/src/components/HabitForm.tsx
+++ client/src/components/HabitForm.tsx
@@ -3,6 +3,7 @@ import { IHabit, createHabit } from '../services/habits';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 
 interface HabitFormProps {
   onSave: () => void;
@@ -10,14 +11,22 @@ interface HabitFormProps {
 
 const HabitForm: React.FC<HabitFormProps> = ({ onSave }) => {
   const [habit, setHabit] = useState<IHabit>({ name: '' });
+  const [error, setError] = useState(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setHabit((prevHabit) => ({ ...prevHabit, [name]: value }));
+    if (e.target.value.trim() !== '') {
+      setError(false);  // Reset error if input is not empty
+    }
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (habit.name.trim() === '') {
+      setError(true);  // Show error if input is empty
+      return
+    }
     await createHabit(habit);
     setHabit({ name: '' });
     onSave();
@@ -28,18 +37,24 @@ const HabitForm: React.FC<HabitFormProps> = ({ onSave }) => {
       component="form"
       sx={{
         display: 'flex',      // Flexbox container
+        alignItems: 'center',
         gap: 1               // Space between elements
       }}
       onSubmit={handleSubmit}
       noValidate
       autoComplete="off"
     >
+      <Typography variant="h4" component="h1" sx={{ mb: 2 }} >
+        Create your habit here:
+      </Typography>
       <TextField
         required
-        label="name"
+        label="New Habit Name"
         name="name"
         value={habit.name}
         onChange={handleChange}
+        error={error}
+        helperText={error ? 'Habit name is required' : ''}  // Show error message
       />
       <Button
         type="submit"
