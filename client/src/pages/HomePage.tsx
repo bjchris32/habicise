5df236ad08ca7a8d82831ddeaa9ac10784c6811f
Chris Shao <bjchris32@gmail.com>
2024-09-09 17:10:28 -0500

    feat(home): put signup/login form in the page

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,7 +1,17 @@
-import React, { useState, useEffect }  from 'react';
-import { Container, Box, Typography } from '@mui/material';
+import React, { useState, useEffect, useContext }  from 'react';
+import { Container, Box, Card, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid2';
+import { AuthContext } from '../contexts/AuthContext';
+import SignUpForm from '../components/SignUpForm'
+import LoginForm from '../components/LoginForm'
 
 const HomePage: React.FC = () => {
+  const authContext = useContext(AuthContext); // Directly use useContext
+  if (!authContext) {
+    throw new Error('LoginForm must be used within an AuthProvider');
+  }
+  const { isLoggedIn, login } = authContext;
+
   return (
     <Container maxWidth={false}>
       <Box
@@ -18,6 +28,30 @@ const HomePage: React.FC = () => {
             Cultivate your new habit in streaks like doing exercise
           </Typography>
         </Typography>
+        {!isLoggedIn ? (
+          <Grid container spacing={2} columns={16}>
+            <Grid size={8}>
+              <Card sx={{ p: 4 }}>
+                <Typography variant="h5" align="center" gutterBottom>
+                  Sign Up
+                </Typography>
+                <SignUpForm onSave={login}/>
+              </Card>
+            </Grid>
+            <Grid size={8}>
+              <Card sx={{ p: 4 }}>
+                <Typography variant="h5" align="center" gutterBottom>
+                  Login
+                </Typography>
+                <LoginForm onSave={login}/>
+              </Card>
+            </Grid>
+          </Grid>
+        ) : (
+          <Typography variant="h5" align="center" sx={{ mt: 4 }}>
+            Welcome!
+          </Typography>
+        )}
       </Box>
     </Container>
   );
