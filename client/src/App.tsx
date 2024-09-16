6f5655f7282d0e2bd034495ddf1ad40a0a1da6a9
Chris Shao <bjchris32@gmail.com>
2024-09-16 14:39:29 -0500

    feat: make the router outside to prevent navigation error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,46 +1,52 @@
-import React, { useState, useEffect, useContext }  from 'react';
+import { useContext }  from 'react';
 import './App.css';
-import HabitList from './components/HabitList';
-import HabitForm from './components/HabitForm';
-import { IHabit, getHabits } from './services/habits';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
 
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import SignUpPage from './pages/SignUpPage';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import HabitListPage from './pages/HabitListPage';
 
 import { AuthContext } from './contexts/AuthContext';
-
+import { useNavigate } from 'react-router-dom';
+import { logoutUser } from './services/authentications-services'
 
 function App() {
+  const navigate = useNavigate();
   const authContext = useContext(AuthContext); // Directly use useContext
   if (!authContext) {
     throw new Error('LoginForm must be used within an AuthProvider');
   }
-  const { isLoggedIn, login, logout } = authContext;
+  const { isLoggedIn, logout } = authContext;
 
   return (
-    <Router>
+    <>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1 }}
+            onClick={() => navigate('/')}>
             Habicise
           </Typography>
           {isLoggedIn ? (
-            <Button color="inherit" onClick={logout}>
+            <Button color="inherit" onClick={ async () => {
+              await logoutUser();
+              logout();
+            }}>
               Logout
             </Button>
           ) : (
             <>
-              <Button color="inherit" onClick={login}>
+              <Button color="inherit" onClick={() => navigate('/login')}>
                 Login
               </Button>
-              <Button color="inherit">Signup</Button>
+              <Button color="inherit" onClick={() => navigate('/signup')}>
+                Signup
+              </Button>
             </>
           )}
         </Toolbar>
@@ -52,7 +58,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/:userId/habits" element={<HabitListPage />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,15 +8,18 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { AuthProvider } from './contexts/AuthContext';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App/>
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App/>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
