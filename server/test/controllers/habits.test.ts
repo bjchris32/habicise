ca5499e30f5623a8dd9f24609f6989bc0439d018
Chris Shao <bjchris32@gmail.com>
2024-09-18 11:00:21 -0500

    test(habits controller): add test cases for a different user

--- server/test/controllers/habits.test.ts
+++ server/test/controllers/habits.test.ts
@@ -20,51 +20,99 @@ beforeEach(async () => {
 });
 
 describe("Habits functions", () => {
-  it("should create a habit document", async () => {
-    const response = await request(app)
-      .post('/api/habits')
-      .send({ name: 'Habit 2' });
-    expect(response.status).toBe(201);
-    expect(response.body.name).toBe('Habit 2');
-    expect(response.body.user).toBe(user.id);
-  });
+  describe("habits are from the same user as auth", () => {
+    it("should create a habit document", async () => {
+      const response = await request(app)
+        .post('/api/habits')
+        .send({ name: 'Habit 2' });
+      expect(response.status).toBe(201);
+      expect(response.body.name).toBe('Habit 2');
+      expect(response.body.user).toBe(user.id);
+    });
 
-  it("should read a habit document", async () => {
-    const habit = new Habit({ name: 'Habit 3', user: user.id });
-    const savedHabit = await habit.save();
-    const response = await request(app).get(`/api/habits/${savedHabit._id}`);
-    expect(response.status).toBe(200);
-    expect(response.body.name).toBe('Habit 3');
-  });
+    it("should read a habit document", async () => {
+      const habit = new Habit({ name: 'Habit 3', user: user.id });
+      const savedHabit = await habit.save();
+      const response = await request(app).get(`/api/habits/${savedHabit._id}`);
+      expect(response.status).toBe(200);
+      expect(response.body.name).toBe('Habit 3');
+    });
 
-  it("should list habit documents", async () => {
-    const habit1 = new Habit({ name: 'Habit 7', user: user.id });
-    const savedHabit1 = await habit1.save();
-    const habit2 = new Habit({ name: 'Habit 8', user: user.id });
-    const savedHabit2 = await habit2.save();
-    const response = await request(app).get(`/api/habits/`);
-    expect(response.status).toBe(200);
-    expect(response.body.length).toBe(2);
-    expect(response.body[0].name).toBe(savedHabit1.name);
-    expect(response.body[1].name).toBe(savedHabit2.name);
-  });
+    it("should list habit documents", async () => {
+      const habit1 = new Habit({ name: 'Habit 7', user: user.id });
+      const savedHabit1 = await habit1.save();
+      const habit2 = new Habit({ name: 'Habit 8', user: user.id });
+      const savedHabit2 = await habit2.save();
+      const response = await request(app).get(`/api/habits/`);
+      expect(response.status).toBe(200);
+      expect(response.body.length).toBe(2);
+      expect(response.body[0].name).toBe(savedHabit1.name);
+      expect(response.body[1].name).toBe(savedHabit2.name);
+    });
 
-  it("should update a habit document", async () => {
-    const habit = new Habit({ name: 'Habit 3', user: user.id });
-    const savedHabit = await habit.save();
-    const response = await request(app)
-      .put(`/api/habits/${savedHabit._id}`)
-      .send({ name: 'Habit not your business' });
-    expect(response.status).toBe(200);
-    expect(response.body.name).toBe ('Habit not your business');
-  });
+    it("should update a habit document", async () => {
+      const habit = new Habit({ name: 'Habit 3', user: user.id });
+      const savedHabit = await habit.save();
+      const response = await request(app)
+        .put(`/api/habits/${savedHabit._id}`)
+        .send({ name: 'Habit not your business' });
+      expect(response.status).toBe(200);
+      expect(response.body.name).toBe ('Habit not your business');
+    });
 
-  it("should delete a habit document", async () => {
-    const habit = new Habit({ name: 'Habit to be removed', user: user.id });
-    const savedHabit = await habit.save();
-    const response = await request(app)
-      .delete(`/api/habits/${savedHabit._id}`)
-    expect(response.status).toBe(200);
-    expect(response.body.message).toBe ('Habit deleted successfully');
+    it("should delete a habit document", async () => {
+      const habit = new Habit({ name: 'Habit to be removed', user: user.id });
+      const savedHabit = await habit.save();
+      const response = await request(app)
+        .delete(`/api/habits/${savedHabit._id}`)
+      expect(response.status).toBe(200);
+      expect(response.body.message).toBe ('Habit deleted successfully');
+    });
+  })
+
+  describe("habits are from another user", () => {
+    let savedAnotherUser: any;
+    beforeEach(async () => {
+      const anotherUser = new User({
+        name: 'second User',
+        email: 'second@gmail.com',
+        password: 'test123'
+      });
+      savedAnotherUser = await anotherUser.save();
+    });
+
+    it("should not read a habit document", async () => {
+      const habit = new Habit({ name: 'Habit 3', user: savedAnotherUser.id });
+      const savedHabit = await habit.save();
+      const response = await request(app).get(`/api/habits/${savedHabit._id}`);
+      expect(response.status).toBe(404);
+    });
+
+    it("should not list habit documents", async () => {
+      const habit1 = new Habit({ name: 'Habit 7', user: savedAnotherUser.id });
+      const savedHabit1 = await habit1.save();
+      const habit2 = new Habit({ name: 'Habit 8', user: savedAnotherUser.id });
+      const savedHabit2 = await habit2.save();
+      const response = await request(app).get(`/api/habits/`);
+      expect(response.status).toBe(200);
+      expect(response.body.length).toBe(0);
+    });
+
+    it("should not update the habit document", async () => {
+      const habit = new Habit({ name: 'Habit 3', user: savedAnotherUser.id });
+      const savedHabit = await habit.save();
+      const response = await request(app)
+        .put(`/api/habits/${savedHabit._id}`)
+        .send({ name: 'Habit not your business' });
+      expect(response.status).toBe(404);
+    });
+
+    it("should not delete a habit document", async () => {
+      const habit = new Habit({ name: 'Habit to be removed', user: savedAnotherUser.id });
+      const savedHabit = await habit.save();
+      const response = await request(app)
+        .delete(`/api/habits/${savedHabit._id}`)
+      expect(response.status).toBe(404);
+    });
   });
 });
